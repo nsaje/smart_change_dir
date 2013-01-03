ac0165ebb251503841749399d3ae6e648707c9bf
Nejc Saje <nejc.saje@gmail.com>
2013-01-03 22:29:59 +0100

    created

---
+++ scd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Uses 'locate' to find the directory you want and 'pushd' to change directories.
+#
+case "$1" in
+	""|"-h"|"--help")
+		echo "Smart Change Directory, changes directory by keywords."
+		echo "Usage: . scd keyword1 keyword2 ..."
+		echo "IMPORTANT: the script must be sourced (the dot before the script name) or else it won't work!"
+		echo "If the wrong directory is found, use the 'popd' command to return to the previous directory."
+		;;
+	*)
+		PATTERN=$(printf "%s*" "$@")
+		FOUND=$(locate *"$PATTERN" | head -n 1)
+		if [ -d "$FOUND" ]; then
+			pushd "$FOUND"
+		fi
+		;;
+esac
